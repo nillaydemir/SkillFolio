3dd609fded24e983e8b80ad0459322ecd654102a
Aylinbaki <aylinbaki@hotmail.com>
2026-01-06 00:28:49 +0300

    düzeltmeler

---
+++ SkillFolio/Migrations/20260105211423_AddIsApprovedAndRegisteredAtToUserEvent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillFolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsApprovedAndRegisteredAtToUserEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "UserEvents",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegisteredAt",
+                table: "UserEvents",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "UserEvents");
+
+            migrationBuilder.DropColumn(
+                name: "RegisteredAt",
+                table: "UserEvents");
+        }
+    }
+}
